8673727045d04fe2464cf8a1d94df2012b91b5b1
pauloJ-r <pj74586@gmail.com>
2023-03-27 20:22:31 -0300

    exe

---
+++ Laços 2023/exercicios nivel 1.js
@@ -0,0 +1,67 @@
+// questões nivel 1 de laços do curso de TSI, usando while
+
+//questão 1 = imprimir de -1 a -20
+var a = -1;
+
+while(a > -20){
+    a--;
+    console.log(a);
+}
+//quetão 2 = imprimir numero de 3 a 3 até a 30
+var a = 0;
+
+while(a <= 30){
+    console.log(a);
+    a+=3;
+}
+////quetão 3 = imprimir numero de 3 a 3 até a -30
+var a = 0;
+
+while(a >= -30){
+    console.log(a);
+    a-=3;
+}
+//questão 4 = imprimir os 10 primeiros numeros impares
+var impar = 1;
+var count = 0;
+
+while(count < 10){
+    console.log(impar);
+    impar+=2;
+    count++;
+}
+// questão 5 = imprimir os 20 numeros divisiveis por 3
+var div = 3;
+var count = 0;
+
+while(count < 20){
+    console.log(div);
+    div+=3;
+    count++;
+}
+//questão 6 = imprimir os 10 numeros divisiveis por 10
+var div = 10;
+var count = 0;
+
+while(count < 10){
+    console.log(div);
+    div+=10;
+    count++;
+}
+//questão 7 = imprimir o quadrado dos 10 primeiros numeros inteiros
+var q = 1;
+var count = 0;
+
+while(count < 10){
+    console.log(q*q);
+    q++;
+    count++;
+    
+}
+//quetão 8 = imprimir de 0 a 25 em descrescente
+var a = 25;
+
+while(a >= 0){
+    console.log(a);
+    a--;
+}
